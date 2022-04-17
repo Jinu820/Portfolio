f94405139460dc16ff02a0071737e327a1816c7f
Jinu820 <96170708+Jinu820@users.noreply.github.com>
2022-04-17 22:10:54 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,22 +1,50 @@
+
+
 var sec1top = $('.sec1').offset().top
-var sec2top = $('.sec2').offset().top
-var sec3top = $('.sec3').offset().top
-var sec4top = $('.sec4').offset().top
+var sec2top = $('.sec2').offset().top-200
+var sec3top = $('.sec3').offset().top-200
+var sec4top = $('.sec4').offset().top-200
 var sct = 0;
 var ww = $(window).height()
 
-function count(jumsu, cname, time) {
-    let count = 0
-    let stop = setInterval(function(){
-        count++
-        if (count<=jumsu) {
-            $(cname).find('.myscore').text(count+'%')
-        } else {
-            clearInterval(stop)
-        }
-    }, time)
-}
+let text = document.querySelector('.typing')
+let typewriter = new Typewriter(text, {
+    loop:true
+})
+typewriter
+    .typeString('Welcome')
+    .pauseFor(0)
+    .typeString('<br/>')
+    .typeString('to my')
+    .pauseFor(1000)
+    .deleteAll()
+    .typeString('Port-')
+    .pauseFor(0)
+    .typeString('<br/>')
+    .typeString('Folio')
+    .pauseFor(4000)
+    .start();
 
+// function count(jumsu, cname, time) {
+//     let count = 0
+//     let stop = setInterval(function(){
+//         count++
+//         if (count<=jumsu) {
+//             $(cname).find('.myscore').text(count+'%')
+//         } else {
+//             clearInterval(stop)
+//         }
+//     }, time)
+// }
+
+$('.sec1 .sec1down > img').on('click',function(e){
+    e.preventDefault()
+    var sec2top = $('.sec2').offset().top
+    $('html, body').stop().animate({
+        scrollTop:sec2top
+    },500)
+    // $(window).scrollTop(sec2top).animate(500)
+})
 
 var flag = false
 $('.header .menu li a').on('click',function(e){
@@ -25,56 +53,228 @@ $('.header .menu li a').on('click',function(e){
     $(this).parent().addClass('on').siblings().removeClass('on')
     var num = $(this).parent().index()
     var secDist = $('section').eq(num).offset().top
+    $('section').eq(num).addClass('on').siblings().removeClass('on')
     $('html, body').stop().animate({
         scrollTop : secDist
     }, 500, function(){
     flag = false
     })
+    
 })
 
 $(window).on('scroll',function(){
+    var currentPercentage = ($(window).scrollTop() / ($(document).outerHeight() - $(window).height())) * 100;
+    
+    console.log(currentPercentage)
+    $('.loadingBar i').css({
+        'top':`${currentPercentage}%`
+    })
+    $('.loadingBar .loadHeight').css({
+        'height':`${currentPercentage-3}%`
+    })
+    
     sct = $(this).scrollTop()
     if (sct>=sec1top && sct<sec2top && !flag ) {
         $('.header .menu li').eq(0).addClass('on').siblings().removeClass('on')
         $('.header').css({display:'none'})
+        $('section').eq(0).addClass('on').siblings().removeClass('on')
+        $('.sec2 .skillcontainer').removeClass('on')
+
     }
     else if (sct>=sec2top && sct<sec3top && !flag  ) {
         $('.header .menu li').eq(1).addClass('on').siblings().removeClass('on')
         $('.header').css({display:'block'})
-        if ( !$('.sec2 .skillcontainer').hasClass('on') ) {
-            $('.sec2 .skillcontainer').addClass('on')
-            count(80, '.html', 20)
-            count(80, '.css', 20)
-            count(60, '.js', 20)
-            count(70, '.jquery', 20)
-            count(30, '.react', 30)
-        }
+        $('section').eq(1).addClass('on').siblings().removeClass('on')
     }
     else if (sct>=sec3top && sct<sec4top && !flag  ) {
+        $('.header').css({display:'block'})
         $('.header .menu li').eq(2).addClass('on').siblings().removeClass('on')
+        $('section').eq(2).addClass('on').siblings().removeClass('on')
+
     }
     else if (sct>=sec4top && !flag ) {
+        $('.header').css({display:'block'})
         $('.header .menu li').eq(3).addClass('on').siblings().removeClass('on')
+        $('section').eq(3).addClass('on').siblings().removeClass('on')
+
+
     }
 })
 
-var offTop = 0;
-$('section').on('mousewheel',function(e,delta){
-    // console.log(delta) : 아래스크롤시 = -1, 위로스크롤시 = +1값 출력
-    if (delta<0) {
-        offTop = $(this).next().offset().top
-    } else if (delta>0) {
-        offTop = $(this).prev().offset().top
-    }
-    $('html').stop().animate({
-        scrollTop:offTop
-    },500)
+$('.sec2 .skill .html5').circleProgress({
+    value: 0 ,
+    size:150,
+})
+$('.sec2 .skill .css').circleProgress({
+    value: 0 ,
+    size:150,
+})
+$('.sec2 .skill .javascript').circleProgress({
+    value: 0 ,
+    size:150,
+})
+$('.sec2 .skill .jQuery').circleProgress({
+    value: 0 ,
+    size:150,
+})
+$('.sec2 .skill .React').circleProgress({
+    value: 0 ,
+    size:150,
+})
+$('.sec2 .skill .ai').circleProgress({
+    value: 0 ,
+    size:150,
 })
 
-// 비행기 다시 움직이게
-$('.sec1 span i').on('click',function(){
-    $(this).css({animation:'imove 1.5s 10 linear'})
+function startgage(){
+    $('.sec2 .skill .html5').circleProgress({
+        value: 0.9,
+        startAngle: 300,
+        size:150,
+        fill:{
+            gradient:["#f12711","#f5af19"],
+            gradientAngle: Math.PI / 2
+        },
+        lineCap:'round',
+        animation:{
+            duration:2200,
+            easing:"swing"
+        }
+    })
+    .on('circle-animation-progress',
+        function(event, progress) {
+            $(this).find('strong').html(Math.round(90 * progress) + '<i>%</i>');
+        })
+    $('.sec2 .skill .css').circleProgress({
+        value: 0.8,
+        startAngle: 300,
+        size:150,
+        fill:{
+            gradient:["#56ccf2","#2f80ed"],
+            gradientAngle: Math.PI / 2
+        },
+        lineCap:'round',
+        animation:{
+            duration:2200,
+            easing:"swing"
+        }
+    }).on('circle-animation-progress',
+    function(event, progress) {
+        $(this).find('strong').html(Math.round(80 * progress) + '<i>%</i>');
+    })
+    $('.sec2 .skill .javascript').circleProgress({
+        value: 0.6,
+        startAngle: 300,
+        size:150,
+        fill:{
+            gradient:["#ffca28","#fffc00"],
+            gradientAngle: Math.PI / 2
+        },
+        lineCap:'round',
+        animation:{
+            duration:2200,
+            easing:"swing"
+        }
+    }).on('circle-animation-progress',
+    function(event, progress) {
+        $(this).find('strong').html(Math.round(60 * progress) + '<i>%</i>');
+    })
+    $('.sec2 .skill .jQuery').circleProgress({
+        value: 0.7,
+        startAngle: 300,
+        size:150,
+        fill:{
+            gradient:["#1e3c72","#2a5298"],
+            gradientAngle: Math.PI / 2
+        },
+        lineCap:'round',
+        animation:{
+            duration:2200,
+            easing:"swing"
+        }
+    })
+    .on('circle-animation-progress',
+    function(event, progress) {
+        $(this).find('strong').html(Math.round(70 * progress) + '<i>%</i>');
+    })
+    $('.sec2 .skill .React').circleProgress({
+        value: 0.7,
+        startAngle: 300,
+        size:150,
+        fill:{
+            gradient:["#2193b0","#6dd5ed"],
+            gradientAngle: Math.PI / 2
+        },
+        lineCap:'round',
+        animation:{
+            duration:2200,
+            easing:"swing"
+        }
+    })
+    .on('circle-animation-progress',
+    function(event, progress) {
+        $(this).find('strong').html(Math.round(70 * progress) + '<i>%</i>');
+    })
+    $('.sec2 .skill .ai').circleProgress({
+        value: 0.9 ,
+        startAngle: 300,
+        size:150,
+        fill:{
+            gradient:["#f7971e","#ffd200"],
+            gradientAngle: Math.PI / 2
+        },
+        lineCap:'round',
+        animation:{
+            duration:2200,
+            easing:"swing"
+        }
+    })
+    .on('circle-animation-progress',
+    function(event, progress) {
+        $(this).find('strong').html(Math.round(90 * progress) + '<i>%</i>');
+    })
+}
+
+// var offTop = 0;
+// $('section').on('mousewheel',function(e,delta){
+//     // console.log(delta) : 아래스크롤시 = -1, 위로스크롤시 = +1값 출력
+//     if (delta<0) {
+//         offTop = $(this).next().offset().top
+//     } else if (delta>0) {
+//         offTop = $(this).prev().offset().top
+//     }
+//     $('html').stop().animate({
+//         scrollTop:offTop
+//     },500)
+// });
+
+// 원형그래프 영역
+
+$('.sec2 .skill .html').circleProgress({
+    value: 0.8,
+    startAngle: 300,
+    size:200,
+    fill:{
+        gradient:["#06afe8","#77c04a"],
+        gradientAngle: Math.PI / 2
+    },
+    animation:{
+        duration:2200,
+        easing:"swing"
+    },
 })
+
+// 슬라이드 영역
+$('.sec3 .slideInner').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed:700,
+    arrows:true,
+    fade:true,
+    prevArrow:'<button class="slick-arrow slick-prev"><i class="fas fa-angle-left"></i></button>',
+    nextArrow:'<button class="slick-arrow slick-next"><i class="fas fa-angle-right"></i></button>',
+})
+
 
 
 
